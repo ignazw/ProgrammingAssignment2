f715f716b852f80136afc0e6779dc23df4362e31
Ignaz W <ignazw@gmail.com>
2015-10-22 08:47:14 +0200

    added initial descriptive comments
    Added descriptive comments to the file, describing what the functions do

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions:
+##   makeCacheMatrix: creates a cacheable matrix from a matrix and stores it in a cache
+##   cacheSolve: inverts a matrix if not in cache yet, otherwise takes inverted matrix from cache
 
-## Write a short comment describing this function
+## creates a cacheable matrix from a matrix and stores it in a cache
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## inverts a matrix if not in cache yet, otherwise takes inverted matrix from cache
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
